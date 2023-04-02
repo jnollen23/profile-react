9572b41c1fefa9d6bc3434f23cabf3a63db0d33b
Jacob Nollen <jacob.nollen@netplanner.com>
2023-04-01 23:49:52 -0400

    found 6 projects to add to my portfolio

--- src/pages/Repos.js
+++ src/pages/Repos.js
@@ -2,6 +2,9 @@ import Repo from "../components/repo";
 import proj2 from '../assets/images/nottingham.png';
 import proj1 from '../assets/images/GroupProj1.jpg';
 import notey from '../assets/images/notey.png';
+import weather from '../assets/images/weather.png';
+import inter from '../assets/images/inter.png';
+import schedule from '../assets/images/schedule.png'
 
 function Repos() {
     return (
@@ -22,20 +25,20 @@ function Repos() {
                     project='https://notey.herokuapp.com/'
                     repo='https://github.com/jnollen23/notey'
                 />
-                <Repo src={proj2}
-                    title='Nottingham'
-                    project='https://nottingham.herokuapp.com/'
-                    repo='https://github.com/jnollen23/Nottingham'
+                <Repo src={weather}
+                    title='Weather App'
+                    project='https://jnollen23.github.io/WeatherApp/'
+                    repo='https://github.com/jnollen23/WeatherApp'
                 />
-                <Repo src={proj2}
-                    title='Nottingham'
-                    project='https://nottingham.herokuapp.com/'
-                    repo='https://github.com/jnollen23/Nottingham'
+                <Repo src={inter}
+                    title='Interview Questions'
+                    project='https://github.com/jnollen23/interview-questionnaire'
+                    repo='https://jnollen23.github.io/interview-questionnaire/'
                 />
-                <Repo src={proj2}
-                    title='Nottingham'
-                    project='https://nottingham.herokuapp.com/'
-                    repo='https://github.com/jnollen23/Nottingham'
+                <Repo src={schedule}
+                    title='Work Scheduler'
+                    project='https://jnollen23.github.io/day-work-calendar/'
+                    repo='https://github.com/jnollen23/day-work-calendar'
                 />
             </div>
         </div>
